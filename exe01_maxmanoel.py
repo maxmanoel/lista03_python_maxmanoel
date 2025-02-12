0c20e47db38f43c876b52a0cf220ad074e3c0e1b
maxmanoel <max329900@gmail.com>
2025-02-11 21:27:11 -0300

    alteracao

---
+++ exe01_maxmanoel.py
@@ -0,0 +1,7 @@
+#peça 2 numeros, se o primeiro numero for maior que 2, exiba primeiro numero, caso contrario exiba o 2 numero primeiro.
+n1 = int(input("Digite o primeiro numero: "))
+n2 = int(input("Digite o segundo numero: "))
+if(n1>n2):
+    print(n1," ",n2)
+else:
+    print(n2,"",n1)
